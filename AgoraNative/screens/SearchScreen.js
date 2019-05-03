432b6c76fcdc24185b4f5b6584644a8e8eacd741
dkurita997 <44449820+dkurita997@users.noreply.github.com>
2019-05-03 15:19:27 -0700

    Search function
    Currently only displays the first result in the array; need to change to display all
    For some reason, after searching, the text needs to change for the results to change

--- AgoraNative/screens/SearchScreen.js
+++ AgoraNative/screens/SearchScreen.js
@@ -1,5 +1,8 @@
 import React from 'react';
+import axios from 'axios';
 import {Image, TextInput, StyleSheet, View, Button} from 'react-native';
+import { Text, ScrollView, TouchableOpacity } from "react-native";
+import { Card, Tile } from 'react-native-elements';
 
 const styles = StyleSheet.create({
   container: {
@@ -14,13 +17,45 @@ export default class SearchScreen extends React.Component {
     title: 'Search for New Courses',
   };
 
-  keywordCheck()
-  {
-    //use API to look at course list and check if keywords are similar
-    //if text input == courses.keywords
+  getCourseList = () =>{
+    var self = this;
+    return axios.get("https://api.thinkific.com/api/public/v1/courses?page=1&limit=25")
+    .then(function(response){
+        self.setState({originalData: response.data});
+    })
+  }
+
+  keywordCheck = () => {
+    newData = this.state.originalData.items.filter((item) => String(item.keywords).includes(this.state.text.toLowerCase()));
+    this.state.searchData.items = newData;
+
+    console.log(this.state.text.toLowerCase());
+    console.log('search done');
+    console.log(this.state.searchData);
+    courses = this.state.searchData.items;
+  }
+
+  state = {
+    originalData: {}, //original data; does not get modified
+    searchData: {},
+    numCourses: {},
+    text: '',
+    gotCourses: false
   }
 
   render() {
+
+    if (!this.state.gotCourses){
+      this.getCourseList();
+      this.state.gotCourses = true;
+      courses = this.state.originalData.items;
+    }
+
+    if (!courses)
+    {
+        return <View/>
+    }
+    
     return (
       <View style={styles.container}>
         <TextInput
@@ -28,13 +63,26 @@ export default class SearchScreen extends React.Component {
           placeholder="Try 'yoga for beginners'"
           onChangeText={(text) => this.setState({text})}
         />
+
+
         <Button
-            onPress={() => keywordCheck()}
+            onPress={text => this.keywordCheck()}
             title="SEARCH"
             color="#1f66b1"
             accessibilityLabel="Search enter"
-          />
-        </View>
-    )
-    } 
+        />
+
+        {/* change to for loop */}
+            <Card
+                title={courses[0].name}
+                image={{uri: courses[0].course_card_image_url}}
+            >
+            <Text style={{marginBottom: 10}}>
+                {courses[0].description}
+            </Text>
+            </Card>
+
+      </View>
+    );
+  } 
 }
