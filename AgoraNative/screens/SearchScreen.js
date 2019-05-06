70a713ef72f9db6b62a5382bb577debe4eac8920
Sam Burns <sburns@scu.edu>
2019-05-06 12:22:36 -0700

    Modified search screen to display all results

--- AgoraNative/screens/SearchScreen.js
+++ AgoraNative/screens/SearchScreen.js
@@ -1,21 +1,31 @@
 import React from 'react';
 import axios from 'axios';
-import {Image, TextInput, StyleSheet, View, Button} from 'react-native';
-import { Text, ScrollView, TouchableOpacity } from "react-native";
-import { Card, Tile } from 'react-native-elements';
+import {Image, TextInput, StyleSheet, View} from 'react-native';
+import { Text, ScrollView, TouchableOpacity, ActivityIndicator } from "react-native";
+import { Card, Button } from 'react-native-elements';
 
 const styles = StyleSheet.create({
   container: {
     alignItems: 'center',
     justifyContent: 'center',
     backgroundColor: 'white'
-  },
+  }
 });
 
 export default class SearchScreen extends React.Component {
   static navigationOptions = {
     title: 'Search for New Courses',
   };
+
+  state = {
+    originalData: {}, //original data; does not get modified
+    searchData: {},
+    searchArray: [],
+    numCourses: {},
+    text: '',
+    gotCourses: false,
+    searchedCourses: false,
+  }
 
   getCourseList = () =>{
     var self = this;
@@ -31,58 +41,78 @@ export default class SearchScreen extends React.Component {
 
     console.log(this.state.text.toLowerCase());
     console.log('search done');
-    console.log(this.state.searchData);
-    courses = this.state.searchData.items;
-  }
-
-  state = {
-    originalData: {}, //original data; does not get modified
-    searchData: {},
-    numCourses: {},
-    text: '',
-    gotCourses: false
+    //console.log(this.state.searchData.items);
+    this.state.searchArray = this.state.searchData.items
+    console.log(this.state.searchArray)
+    this.setState({searchedCourses: true})
   }
 
   render() {
 
+    const result = this.state.searchArray.map(result => {
+      return (
+        <TouchableOpacity key = {result.id} onPress={() => this.props.navigation.navigate('TakeCourse', {courseID: enrollment.id, courseName: enrollment.name})}>
+          <Card
+            title={result.name}
+            image={{uri: result.course_card_image_url}}
+          >   
+            <Text style={{marginBottom: 10}}>
+              {result.description}
+            </Text>
+          </Card>
+        </TouchableOpacity>
+      )
+    })
     if (!this.state.gotCourses){
       this.getCourseList();
       this.state.gotCourses = true;
-      courses = this.state.originalData.items;
     }
+
+    courses = this.state.originalData.items;
 
     if (!courses)
     {
-        return <View/>
+        return <ActivityIndicator size="large" color="#fff" />
     }
-    
-    return (
-      <View style={styles.container}>
-        <TextInput
-          style={{height: 50, borderColor: 'gray', padding: 15}}
-          placeholder="Try 'yoga for beginners'"
-          onChangeText={(text) => this.setState({text})}
-        />
 
+    console.log(this.state.searchedCourses)
 
-        <Button
+    if (!this.state.searchedCourses)
+    {
+      return (
+        <View style={{flex: 1}}>
+          <TextInput
+            style={{height: 50, borderColor: 'gray', padding: 15}}
+            placeholder="Try 'yoga for beginners'"
+            onChangeText={(text) => this.setState({text})}
+          />
+          <Button
             onPress={text => this.keywordCheck()}
             title="SEARCH"
             color="#1f66b1"
             accessibilityLabel="Search enter"
         />
-
-        {/* change to for loop */}
-            <Card
-                title={courses[0].name}
-                image={{uri: courses[0].course_card_image_url}}
-            >
-            <Text style={{marginBottom: 10}}>
-                {courses[0].description}
-            </Text>
-            </Card>
-
-      </View>
+        </View>
+      );
+      
+    }
+    
+    return (
+      <ScrollView style={{flex: 1}}>
+          <TextInput
+            style={{height: 50, borderColor: 'gray', padding: 15}}
+            placeholder="Try 'yoga for beginners'"
+            onChangeText={(text) => this.setState({text})}
+          />
+          <Button
+            onPress={text => this.keywordCheck()}
+            title="SEARCH"
+            color="#1f66b1"
+            accessibilityLabel="Search enter"
+            style={styles}
+        />
+        {result}
+      </ScrollView>
     );
   } 
 }
